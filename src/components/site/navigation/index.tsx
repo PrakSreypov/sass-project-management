5ef227a8ed6265e38fc36c93f979c61208ebe0fa
Prak Sreypov <sreypovprak88@gmail.com>
2024-12-11 14:56:19 +0700

    feat: add nav bar

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -1,8 +1,15 @@
+import { ModeToggle } from "@/components/global/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Navigation = () => {
+type Props = {
+  user?: null | User;
+};
+
+const Navigation = ({ user }: Props) => {
   return (
     <div className="p-4 flex items-center justify-between relative">
       <aside className="flex items-center gap-2">
@@ -16,7 +23,7 @@ const Navigation = () => {
         <span className="text-xl font-bold">Plura.</span>
       </aside>
 
-      <nav className="hidden md:block absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
+      <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
         <ul className="flex items-center justify-center gap-8">
           <Link href={"#"}>Pricing</Link>
           <Link href={"#"}>About</Link>
@@ -24,6 +31,16 @@ const Navigation = () => {
           <Link href={"#"}>Features</Link>
         </ul>
       </nav>
+
+      <aside className="flex gap-2 items-center">
+        <Link
+          href={"/agency"}
+          className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80">
+          Login
+        </Link>
+        <UserButton />
+        <ModeToggle />
+      </aside>
     </div>
   );
 };
